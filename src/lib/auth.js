cf8463eff873d8ea43ea9ce9ccaf7bd46f85c8df
Shipan Ahmed <shipanahmed1999@gmail.com>
2026-05-21 23:54:38 +0600

    added google feature

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -13,5 +13,11 @@ export const auth = betterAuth({
     }),
     emailAndPassword: {
         enabled: true,
-    }
+    },
+    socialProviders: {
+        google: {
+            clientId: process.env.AUTH_CLIENT_ID ,
+            clientSecret: process.env.AUTH_CLIENT_SECRET,
+        },
+    },
 });
